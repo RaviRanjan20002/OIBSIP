423f7d7ef361821e4d696d0cf6432a0dda2ad81b
RaviRanjan20002 <111779935+RaviRanjan20002@users.noreply.github.com>
2023-04-07 22:52:42 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const Header = () => {
+  return (
+    <>
+    <nav>
+    <h1>Ravi Ranjan</h1>
+    <div className='nav'>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>service</li>
+        <li>info</li>
+    </div>
+    </nav>
+    </>
+  )
+}
+
+export default Header
